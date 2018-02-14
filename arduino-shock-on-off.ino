9c1142128f6b52cfbb83637adccdf6d40fffc6a7
Kenneth Chu <kennth.chu@gmail.com>
2018-02-14 23:34:33 +0800

    Change to count the duration again if shock when switch on

--- arduino-shock-on-off.ino
+++ arduino-shock-on-off.ino
@@ -1,3 +1,5 @@
+#include <Time.h>
+
 // constants won't change. Used here to set a pin number:
 const int ledPin =  LED_BUILTIN;  // the number of the LED pin
 
@@ -6,24 +8,30 @@ const int inPin = 7;
 
 // Variables will change:
 int openSeconds = 3;  // duration to switch on
-
-
+long lastShockTime = 0;
 
 void setup() {
   Serial.begin(9600);
-  
+
   // set the digital pin as output:
   pinMode(ledPin, OUTPUT);
   pinMode(inPin, INPUT);
+
 }
 
 void loop() {
-    int val = digitalRead(inPin);
-    if (val == 1) {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("Open!!!");
-      delay(1000 * openSeconds);
-      digitalWrite(ledPin, LOW);
-      Serial.println("Close!!!");
-    }
+  int val = digitalRead(inPin);
+  if (val == 1) {
+    lastShockTime = millis();
+
+    //Turn on the led
+    Serial.println("Open!!!");
+    digitalWrite(ledPin, HIGH);
+  }
+
+  if ((lastShockTime + 1000 * 3) < millis()) {
+    //Turn off the led
+    Serial.println("Close!!!");
+    digitalWrite(ledPin, LOW);
+  }
 }
